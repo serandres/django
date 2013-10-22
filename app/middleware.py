24bb3c56634de1fb1cdd4e51101c906bbd40be54
serandres <sergiohurtadoramirez@gmail.com>
2013-10-21 23:30:42 -0500

    video3-min-1:24

---
+++ app/middleware.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from random import choice
+
+paises = ['Colombia', 'Peru', 'Panama', 'Mexico']
+
+def de_donde_vengo(request):
+	return choice(paises)
+
+class PaisMiddleWare():
+	def process_request(self,request):
+		pais = de_donde_vengo(request)
+		if pais == 'Mexico':
+			return redirect('http://mejorando.la')
